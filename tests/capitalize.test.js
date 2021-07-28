c629d7cce1c69237b1f1d47a78c4e2f4f596b1fe
Maxim Roganov <mroganov@dclab.local>
2021-07-28 14:49:48 +0300

    add assert to test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
 import capitalize from "../src/capitalize.js";
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'),'Hello');
+assert.equal(capitalize(''),'');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
 
 console.log('Все тесты пройдены!');
